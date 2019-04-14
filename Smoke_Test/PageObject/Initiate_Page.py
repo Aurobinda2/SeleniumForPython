ff85d18908369883365571c3c5330a9216b572d5
Aurobinda <aurobindr.nayak3@gmail.com>
2019-04-15 02:07:59 +0530

    Veryfy Element code updated on 15-04-2019

--- Smoke_Test/PageObject/Initiate_Page.py
+++ Smoke_Test/PageObject/Initiate_Page.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 class Initaite:
-    username_parameter='usename'
+    username_parameter='username'
     password_parameter='password'
     login_button_id='tdb1'
     def __init__(self,driver):
@@ -13,6 +13,7 @@ class Initaite:
 
     def set_username(self,username):
         self.driver.find_element_by_name(self.username_parameter).send_keys(username)
+
 
     def set_password(self,password):
         self.driver.find_element_by_name(self.username_parameter).send_keys(password)
